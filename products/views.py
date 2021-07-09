a3a0fed4daaed8886ac2b5441cf9d91fa66bf2a1
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-07-09 19:20:15 +0900

    UDATE 3

--- products/views.py
+++ products/views.py
@@ -13,31 +13,14 @@ class ProductsView(View):
             efficacy = request.GET.getlist('efficacy',None)
             q_object = Q()
             if efficacy:
-                for i in efficacy:                    
-                    q_object.add(Q(efficacy__id=i),Q.OR)
-
+                q_object &= Q(efficacy__id__in = efficacy)
+             
             products = Product.objects.filter(q_object).distinct()
-
-            # results = [
-            #     {
-            #         "products" : [
-            #             {
-            #                 "productID"          : product.id,
-            #                 "productName"        : product.name,
-            #                 "productPrice"       : product.price,
-            #                 "productTablet"      : product.tablet,
-            #                 "thumbnail_image_url": product.thumbnail_image_url,
-            #                 "icon_image_url"     : [product_icon.icon_url for product_icon in product.efficacy.all()],
-            #                 "summary"          : [text.summary for text in product.productsummary_set.all()]
-            #             }for product in products
-            #         ] 
-            #     }
-            # ]
 
             results = [{
                             "productID"          : product.id,
                             "productName"        : product.name,
-                            "productPrice"       : product.price,
+                            "productPrice"       : int(product.price),
                             "productTablet"      : product.tablet,
                             "thumbnail_image_url": product.thumbnail_image_url,
                             "icon_image_url"     : [product_icon.icon_url for product_icon in product.efficacy.all()],
@@ -63,7 +46,7 @@ class ProductsDetailsView(View):
                     "thumbnail_image_url": product.thumbnail_image_url,
                     "productDescription" : product.description,
                     "icon_image_url"     : [product_icon.icon_url for product_icon in product.efficacy.all()],
-                    "icon_name"          : [product_icon.name for product_icon in product.productsummary_set.all()],
+                    "icon_name"          : [product_icon.summary for product_icon in product.productsummary_set.all()],
                 }
             ]
             return JsonResponse({"message":results},status=200)
