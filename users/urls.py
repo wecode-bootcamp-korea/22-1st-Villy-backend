d5bcb64219070c3023b5d72dd99febef58d3930d
ty <kim@tyui-MacBookAir.local>
2021-07-09 11:34:16 +0900

    ADD: users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import SignupView
+from users.views import SignInView, SignupView
 
 urlpatterns = [
-    path('/signup', SignupView.as_view())
+    path('/signup', SignupView.as_view()),
+    path('/signin', SignInView.as_view())
 ]
