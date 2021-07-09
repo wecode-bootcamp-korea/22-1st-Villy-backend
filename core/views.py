3d828cfe6498dd208312702f96aab96b26a7096a
choi myungjun <junlegend82@gmail.com>
2021-07-09 12:10:46 +0900

    Add: 로그인 체크 decorator

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import jwt
 
-# Create your views here.
+from django.http import JsonResponse
+
+from my_settings import SECRET_KEY, ALGORITHM
+from users.models import User 
+
+def check_login(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization')
+            payload = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+            user = User.objects.get(id = payload['id'])
+            request.user = user
+    
+            return func(self, request, *args, **kwargs)
+        
+        except jwt.DecodeError:
+            return JsonResponse({'message': 'INVAILD_USER'}, status=400)
+
+    return wrapper
