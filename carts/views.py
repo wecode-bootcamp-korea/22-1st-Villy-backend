e7fbc87bf8d1057a0bb581e73c951f3838530b55
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-07-09 11:40:11 +0900

    ADD : carts/views.py

--- carts/views.py
+++ carts/views.py
@@ -1,18 +1,27 @@
 import json
-from datetime       import datetime
+from datetime          import datetime
 
-from django.http    import JsonResponse
-from django.views   import View
+from django.http       import JsonResponse
+from django.views      import View
 
-from productsmodels        import Product, ProductEfficacy, ProductSummary, 
-from .models        import Cart
+from productsmodels    import Product, ProductEfficacy, ProductSummary, 
+from .models           import Cart
 
-# class ProductsView(View):
-#     def get(self, request):
-#         try: 
-#             eye = request.GET.get('eye','')
-#             energy = request.GET.get('energy','')
-#             blood = request.GET.get('blood','')
-#             skin = request.GET.get('skin','')
+def non_user_accept_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try: 
+            access_token = request.headers.get('Authorization', None)
+            if not access_token: 
+                request.user = None 
+                return func(self, request, *args, **kwargs)
+            payload = jwt.decode(access_token, SECRET, algorithms=ALGORITHM)
+            login_user = User.objects.get(id=payload['id'])            
+            request.user = login_user
+            return func(self, request, *args, **kwargs)
+    return wrapper
+
+class ProductsView(View):
+    @non_user_accept_decorator
+    def post(self, request):
+        try: 
             
-#             products = Product.objects.all()
