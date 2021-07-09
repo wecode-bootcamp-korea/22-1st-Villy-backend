9419204fd7033fb51526ff206b3135188cff4ffe
choi myungjun <junlegend82@gmail.com>
2021-07-09 12:30:01 +0900

    Fix: 컨벤션 수정

--- core/views.py
+++ core/views.py
@@ -2,15 +2,15 @@ import jwt
 
 from django.http import JsonResponse
 
-from my_settings import SECRET_KEY, ALGORITHM
+from my_settings  import SECRET_KEY, ALGORITHM
 from users.models import User 
 
 def check_login(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             access_token = request.headers.get('Authorization')
-            payload = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
-            user = User.objects.get(id = payload['id'])
+            payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+            user         = User.objects.get(id = payload['id'])
             request.user = user
     
             return func(self, request, *args, **kwargs)
