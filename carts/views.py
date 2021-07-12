c8a741c2cccc3a449122381a625c6a653e5a759e
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-07-12 22:56:11 +0900

    UPDATED : carts.views & carts.urls

--- carts/views.py
+++ carts/views.py
@@ -14,7 +14,6 @@ from users.models      import User, Point
 from products.models   import Product, ProductEfficacy, ProductSummary, Efficacy
 from .models           import Cart
 
-
 class CartView(View):
     @check_login
     def post(self, request):
@@ -22,9 +21,13 @@ class CartView(View):
             data    = json.loads(request.body)
             user    = request.user
             product = Product.objects.get(id=data['productID'])
-
-            Cart.objects.create(user_id=user.id, product_id=product.id)
-
+            if Product.objects.get(id=data['productID']): 
+                if not Cart.objects.filter(user_id=user.id, product_id=product.id).exists():
+                    Cart.objects.create(user_id=user.id, product_id=product.id)
+                else: 
+                    return JsonResponse({"message":f'{product}_IS_ALREADY_EXISTS'},status=400)
+            else:
+                return JsonResponse({'message' : 'PRODUCT_NOT_EXISTS'}, status=400)
             return JsonResponse({'message' : 'SUCCESS'}, status=200)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
@@ -37,6 +40,7 @@ class CartView(View):
             user  = request.user
             carts = Cart.objects.filter(user_id=user.id)
             results = [{
+                "productID"        : cart.product.id,
                 "productName"        : cart.product.name,
                 "quantity"           : cart.quantity, 
                 "productPrice"       : int(cart.product.price),
@@ -45,7 +49,6 @@ class CartView(View):
             point = Point.objects.get(user=user).point
 
             return JsonResponse({"product":results,"point":point},status=200)
-
         except TypeError:
             return JsonResponse({"message":"TYPE_ERROR"},status=400)
         except ValueError:
@@ -68,18 +71,16 @@ class CartView(View):
         except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
 
+class CartDeleteView(View):
     @check_login
-    def delete(self, request):
+    def delete(self, request, product_id):
         try: 
-            data       = json.loads(request.body)
             user       = request.user
-            product_id = data["productID"]
-
-            for product in product_id:
-                if Cart.objects.filter(product_id=product, user_id=user).exists():
-                    Cart.objects.filter(product_id=product, user_id=user).delete()
-                else: 
-                    return JsonResponse({"message":f'{product}_IS_NOT_FOUND'},status=204)
+            product = Product.objects.get(id=product_id)            
+            if Cart.objects.filter(product=product, user=user).exists():
+                Cart.objects.filter(product=product, user=user).delete()
+            else: 
+                return JsonResponse({"message":f'{product}_IS_NOT_FOUND'},status=204)
             return JsonResponse({"message":"DELETE_COMPLETED"},status=204)
         except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
