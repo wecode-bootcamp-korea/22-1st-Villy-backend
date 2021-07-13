6cd012a7eb5d60524758ebc35e811c2ccddd5795
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-07-13 13:02:27 +0900

    UPDATED : carts.views

--- carts/views.py
+++ carts/views.py
@@ -17,28 +17,32 @@ from .models           import Cart
 class CartView(View):
     @check_login
     def post(self, request):
-        try: 
-            data    = json.loads(request.body)
-            user    = request.user
-            product = Product.objects.get(id=data['productID'])
-            if Product.objects.get(id=data['productID']): 
-                if not Cart.objects.filter(user_id=user.id, product_id=product.id).exists():
-                    Cart.objects.create(user_id=user.id, product_id=product.id)
-                else: 
-                    return JsonResponse({"message":f'{product}_IS_ALREADY_EXISTS'},status=400)
-            else:
-                return JsonResponse({'message' : 'PRODUCT_NOT_EXISTS'}, status=400)
+        try:             
+            data                    = json.loads(request.body)
+            user                    = request.user
+            product                 = Product.objects.get(id=data['productID'])
+            cart_object, cart_exist = Cart.objects.get_or_create(user_id=user.id, product_id=product.id)
+
+            if not cart_exist:
+                return JsonResponse({"message":f'{product.id}_IS_ALREADY_EXISTS'},status=400)
             return JsonResponse({'message' : 'SUCCESS'}, status=200)
+            
+        except ObjectDoesNotExist:
+            return JsonResponse({'message' : 'MODEL_ERROR'}, status=400)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
         except ValueError:
             return JsonResponse({'message' : 'VALUE_ERROR'}, status=400)
+        except TypeError:
+            return JsonResponse({"message":"TYPE_ERROR"},status=400)
+
 
     @check_login
     def get(self, request):
         try: 
             user  = request.user
             carts = Cart.objects.filter(user_id=user.id)
+
             results = [{
                 "productID"        : cart.product.id,
                 "productName"        : cart.product.name,
@@ -46,9 +50,12 @@ class CartView(View):
                 "productPrice"       : int(cart.product.price),
                 "thumbnail_image_url": cart.product.thumbnail_image_url,
             }for cart in carts]
-            point = Point.objects.get(user=user).point
 
+            point = Point.objects.get(user=user).point
             return JsonResponse({"product":results,"point":point},status=200)
+
+        except ObjectDoesNotExist:
+            return JsonResponse({'message' : 'MODEL_ERROR'}, status=400)
         except TypeError:
             return JsonResponse({"message":"TYPE_ERROR"},status=400)
         except ValueError:
@@ -61,26 +68,40 @@ class CartView(View):
             user       = request.user
             product_id = data["productID"]
             product    = Product.objects.get(id=product_id)
+            cart       = Cart.objects.filter(product_id=product, user_id=user)
 
-            if Cart.objects.filter(product_id=product, user_id=user).exists():
-                Cart.objects.filter(product_id=product, user_id=user).update(quantity = data["quantity"])
-            else: 
-                return JsonResponse({"message":f'{product_id}_IS_NOT_FOUND'},status=400)
-                
-            return JsonResponse({"message":"UPDATE_COMPLETED"},status=200)
+            if cart.exists():
+                cart.update(quantity = data["quantity"])
+                return JsonResponse({"message":"UPDATE_COMPLETED"},status=200) 
+            return JsonResponse({"message":f'{product_id}_IS_NOT_FOUND'},status=400)
+
+        except ObjectDoesNotExist:
+            return JsonResponse({'message' : 'MODEL_ERROR'}, status=400)
         except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
+        except ValueError:
+            return JsonResponse({"message":"VALUE_ERROR"},status=400)
+        except TypeError:
+            return JsonResponse({"message":"TYPE_ERROR"},status=400)
 
 class CartDeleteView(View):
     @check_login
     def delete(self, request, product_id):
         try: 
-            user       = request.user
-            product = Product.objects.get(id=product_id)            
-            if Cart.objects.filter(product=product, user=user).exists():
-                Cart.objects.filter(product=product, user=user).delete()
-            else: 
-                return JsonResponse({"message":f'{product}_IS_NOT_FOUND'},status=204)
-            return JsonResponse({"message":"DELETE_COMPLETED"},status=204)
+            user    = request.user
+            product = Product.objects.get(id=product_id)
+            cart    = Cart.objects.get(product=product, user=user)
+
+            if cart: 
+                cart.delete()
+                return JsonResponse({"message":"DELETE_COMPLETED"},status=204) 
+            return JsonResponse({"message":f'{product}_IS_NOT_FOUND'},status=400)
+
+        except ObjectDoesNotExist:
+            return JsonResponse({'message' : 'MODEL_ERROR'}, status=400)
         except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
+        except TypeError:
+            return JsonResponse({"message":"TYPE_ERROR"},status=400)
+        except ValueError:
+            return JsonResponse({"message":"VALUE_ERROR"},status=400)
